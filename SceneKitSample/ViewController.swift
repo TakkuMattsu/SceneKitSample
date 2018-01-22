c0884b7579348f526ddfb56eef0df434f96a14e4
TakkuMatssu <takku.mattsu@gmail.com>
2018-01-22 15:19:54 +0900

    その3まで

--- SceneKitSample/ViewController.swift
+++ SceneKitSample/ViewController.swift
@@ -19,6 +19,26 @@ class ViewController: UIViewController {
             scnView.scene = scene
             scnView.showsStatistics = true
             scnView.backgroundColor = UIColor.black
+            scnView.allowsCameraControl = true
+            // シーンを追加してみる
+            let sphere: SCNGeometry = SCNSphere(radius: 2)
+            //sphere.firstMaterial?.diffuse.contents = UIColor.blue
+            sphere.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Image")
+            let geometryNode = SCNNode(geometry: sphere)
+            // 回してみる
+            geometryNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 2)))
+            scene.rootNode.addChildNode(geometryNode)
+            // カメラ追加
+            let cameraNode = SCNNode()
+            cameraNode.camera = SCNCamera()
+            cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+            scene.rootNode.addChildNode(cameraNode)
+            // ライト
+            let lightNode = SCNNode()
+            lightNode.light = SCNLight()
+            lightNode.light!.type = .omni
+            lightNode.position = SCNVector3(x: 10, y: 0, z: 10)
+            scene.rootNode.addChildNode(lightNode)
         }
     }
 
